18d17b19db1f4fb03bdc6a20483da9cd10b4bfda
Marc DellaVolpe <marc@cureatr.com>
2018-03-09 11:15:15 -0500

    simplify

--- scanner.sh
+++ scanner.sh
@@ -7,12 +7,12 @@ HIGH="1700M"
 STEP="125k"
 DURATION="10m"
 
-OPTS="-P -D -O"
+OPTS=""
 
 while true; do
     NOW=`date`
     STAMP=`date +%Y%m%d%H%M%S`
     echo "[${NOW}] Scanning ${LOW} -> ${HIGH} with step ${STEP} for ${DURATION}..."
-    rtl_power -f ${LOW}:${HIGH}:${STEP} -e ${DURATION} ${OPS} 2>rtl_power.log |gzip >./scans/tmp.csv.gz
-    mv ./scans/tmp.csv.gz ./scans/scan-${LOW}-${HIGH}-${STAMP}.csv.gz
+    rtl_power -f ${LOW}:${HIGH}:${STEP} -e ${DURATION} ${OPS} 2>rtl_power.log |gzip > /tmp/scan.csv.gz
+    mv /tmp/scan.csv.gz ./scans/scan-${LOW}-${HIGH}-${STAMP}.csv.gz
 done
