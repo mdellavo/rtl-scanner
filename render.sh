5d6c5d82c2ba863aa02e0e3a42ad1f240986d86d
Marc DellaVolpe <marc@cureatr.com>
2018-03-09 12:28:47 -0500

    touch ups

--- render.sh
+++ render.sh
@@ -2,6 +2,8 @@
 
 #set -x
 
+rsync -avz $HOST:~/scans/scan* scans/
 cat $(ls ./scans/scan-*.csv.gz) | gunzip > /tmp/scan.csv
 python ./heatmap.py /tmp/scan.csv render.png
 rm /tmp/scan.csv
+open render.png
--- scanner.sh
+++ scanner.sh
@@ -6,13 +6,12 @@ LOW="24M"
 HIGH="1700M"
 STEP="125k"
 DURATION="10m"
-
-OPTS=""
+ARGS="-g 50"
 
 while true; do
     NOW=`date`
     STAMP=`date +%Y%m%d%H%M%S`
     echo "[${NOW}] Scanning ${LOW} -> ${HIGH} with step ${STEP} for ${DURATION}..."
-    rtl_power -f ${LOW}:${HIGH}:${STEP} -e ${DURATION} ${OPS} 2>rtl_power.log |gzip > /tmp/scan.csv.gz
+    rtl_power -f ${LOW}:${HIGH}:${STEP} -e ${DURATION} ${ARGS} 2>rtl_power.log |gzip > /tmp/scan.csv.gz
     mv /tmp/scan.csv.gz ./scans/scan-${LOW}-${HIGH}-${STAMP}.csv.gz
 done
