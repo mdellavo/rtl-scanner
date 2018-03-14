a0a3eeecf1e46270276cb92d8ff77d34c6c1b6b4
Marc DellaVolpe <marc@cureatr.com>
2018-03-14 16:31:00 -0400

    updates

--- render.sh
+++ render.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #set -x
+set -o pipefail
 
 if [ ! -f ./heatmap.py ]; then
     wget https://raw.githubusercontent.com/keenerd/rtl-sdr-misc/master/heatmap/heatmap.py
 fi
 
-rsync -avz $HOST:~/scans/scan* scans/
+rsync -avz --delete $HOST:~/scans/scan-*.csv.gz scans/
 cat $(ls ./scans/scan-*.csv.gz) | gunzip > /tmp/scan.csv
 python ./heatmap.py --palette charolastra /tmp/scan.csv render.png
 rm /tmp/scan.csv
--- scanner.sh
+++ scanner.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 
 # set -x 
+set -o pipefail
 
-LOW="24M"
-HIGH="1700M"
-STEP="125k"
-DURATION="10m"
-ARGS="-g 50"
+ARGS=3
+if [ $# -lt ${ARGS} ]
+then
+    echo "Usage: `basename $0` LOW HIGH STEP [DURATION]"
+    exit 1
+fi
+
+LOW="$1"
+HIGH="$2"
+STEP="$3"
+DURATION="$4"
+
+if [ -z ${DURATION} ];
+then
+    DURATION="10m"
+fi
 
 if [ ! -d scans ]; then
     mkdir scans
 fi
 
+if [ ! -d scans/archive ]; then
+    mkdir scans/archive
+fi
+
+mv scans/scan-*.csv.gz scans/archive
+
+trap exit EXIT
+
+echo "[`date`] Scanning ${LOW} -> ${HIGH} with step ${STEP} for ${DURATION} increments..."
 while true; do
-    NOW=`date`
-    STAMP=`date +%Y%m%d%H%M%S`
-    echo "[${NOW}] Scanning ${LOW} -> ${HIGH} with step ${STEP} for ${DURATION}..."
+    STAMP=`date +%Y-%m-%d-%H-%M-%S`
     rtl_power -f ${LOW}:${HIGH}:${STEP} -e ${DURATION} ${ARGS} 2>rtl_power.log |gzip > /tmp/scan.csv.gz
-    mv /tmp/scan.csv.gz ./scans/scan-${LOW}-${HIGH}-${STAMP}.csv.gz
+    if [ $? -ne 0 ];
+    then
+        echo "error running rtl_power"
+        echo
+        cat rtl_power.log
+        exit
+    fi;
+    mv /tmp/scan.csv.gz scans/scan-${LOW}-${HIGH}-${STAMP}-${DURATION}.csv.gz
 done
