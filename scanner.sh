5924f710804f2bef58817f6224d83b8168abf193
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2018-03-16 21:55:28 -0400

    updates

--- scanner.sh
+++ scanner.sh
@@ -19,28 +19,17 @@ then
     DURATION="10m"
 fi
 
-if [ ! -d scans ]; then
-    mkdir scans
-fi
-
 if [ ! -d scans/archive ]; then
-    mkdir scans/archive
+    mkdir -p scans/archive
 fi
 
-mv scans/scan-*.csv.gz scans/archive
+mv -f scans/scan-*.csv.gz scans/archive
 
 trap exit EXIT
 
-echo "[`date`] Scanning ${LOW} -> ${HIGH} with step ${STEP} for ${DURATION} increments..."
+echo "[`date`] Scanning ${LOW} -> ${HIGH} with step ${STEP} for ${DURATION} increments (ARGS=${ARGS})..."
 while true; do
     STAMP=`date +%Y-%m-%d-%H-%M-%S`
-    rtl_power -f ${LOW}:${HIGH}:${STEP} -e ${DURATION} ${ARGS} 2>rtl_power.log |gzip > /tmp/scan.csv.gz
-    if [ $? -ne 0 ];
-    then
-        echo "error running rtl_power"
-        echo
-        cat rtl_power.log
-        exit
-    fi;
+    rtl_power -f ${LOW}:${HIGH}:${STEP} -e ${DURATION} ${ARGS} |gzip > /tmp/scan.csv.gz
     mv /tmp/scan.csv.gz scans/scan-${LOW}-${HIGH}-${STAMP}-${DURATION}.csv.gz
 done
